337c7f25c788b7b30b667030f0be1267382c47ab
Lorenzo Santi <lorenzosanti@host-003.homenet.telecomitalia.it>
2013-09-05 16:22:21 +0200

    Initial Commit

---
+++ DMLocalizedString/DMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DMAppDelegate.h
+//  DMLocalizedString
+//
+//  Created by Lorenzo Santi on 05/09/13.
+//  Copyright (c) 2013 DM Digital s.r.l. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DMLocalizedString/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DMLocalizedString
+//
+//  Created by Lorenzo Santi on 05/09/13.
+//  Copyright (c) 2013 DM Digital s.r.l. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DMAppDelegate class]));
+    }
+}
